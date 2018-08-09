e4f99d8d0eda589c46537623fc83359c4fc45778
cyan <cncyan@163.com>
2018-08-09 08:09:56 +0800

    axios

--- tranvel/src/pages/home/Home.vue
+++ tranvel/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
   <home-header></home-header>
   <home-swiper></home-swiper>
   <home-icon></home-icon>
+  <home-recommon></home-recommon>
 </div>
 </template>
 
@@ -10,12 +11,25 @@
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/Swiper'
 import HomeIcon from './components/Icons'
+import HomeRecommon from './components/Recommon'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcon
+    HomeIcon,
+    HomeRecommon
+  },
+  methods:{
+    getHomeInfo(){
+      axios.get('/static/mock/index.json').then(function(res){
+        console.log(res.data)
+      })
+    }
+  },
+  mounted(){
+    this.getHomeInfo()
   }
 }
 </script>
---
+++ tranvel/src/pages/home/components/Recommon.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <div class="title-recommon">重要重要</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Recommon"
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
